4ff87c3f2d82fe932ca5dfc71f762f04a9a2f924
gabriela peratello <gabriela.p.silva10@aluno.senai.br>
2026-03-06 17:10:26 -0300

    Arrumando os testes

--- escola.py
+++ escola.py
@@ -1,13 +1,18 @@
 def verificar_media(media:int|float) -> str:
     """Essa função tem como objetivo retornar se o aluno passou ou não de ano"""
-    
-    if media >=7:
+
+    # Testando para ver se a média é um número
+    if not isinstance(media, int):
+        raise TypeError ("Tipo inválido, a entrada deve ser numérica.")
+
+    if media >=7: 
         return "Aprovado"
-    elif media < 5:
+    elif media <= 4: 
         return "Reprovado"
-    else:
+    else: 
         return "Recuperação"
+    
 
 
 if __name__ == "__main__":
-    print(verificar_media(4))
+    print(verificar_media(6))
--- test/test_media_erros.py
+++ test/test_media_erros.py
@@ -3,5 +3,15 @@ from escola import verificar_media
 
 
 def test_string():
-     with pytest.raises(TypeError, match="Tipo iválido, a entrada deve sernumérica."):
+     with pytest.raises(TypeError, match="Tipo inválido, a entrada deve ser numérica."):
           verificar_media("OITO")
+
+
+def test_numero_negativo():
+     with pytest.raises(ValueError, match="O valor deve ser maior ou igual a 0 e menor ou igual a 10."):
+          verificar_media(-5)
+
+
+def test_maior_10():
+     with pytest.raises(ValueError, match="O valor deve ser menor ou igual a 0 e menor ou igual a 10."):
+          verificar_media
